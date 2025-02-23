7826af6bdfcc241c99ca7067fc87e718f96416b7
nangdypanhar <181545922+nangdypanhar@users.noreply.github.com>
2025-02-23 16:56:47 +0700

    Init Project

---
+++ lib/service/locations_service.dart
@@ -0,0 +1,12 @@
+import 'package:bla_bla_project/model/ride/locations.dart';
+
+import '../dummy_data/dummy_data.dart';
+
+////
+///   This service handles:
+///   - The list of available rides
+///
+class LocationsService {
+  static const List<Location> availableLocations =
+      fakeLocations; // TODO for now fake data
+}
